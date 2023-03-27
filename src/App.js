ab40fd9b0b2ea592acd597c248745bb6fd1342d5
Tapwatero <charliewoolf20@gmail.com>
2023-03-27 23:54:17 +0100

    stylistic changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './output.css';
 import './input.css';
 
@@ -19,9 +19,10 @@ const Socket = () => {
     }
 
 
-
     // Get Messages
     useEffect(() => {
+
+
         const socket = new WebSocket('wss://cloaked-backend.onrender.com');
         setSocket(socket);
 
@@ -32,12 +33,17 @@ const Socket = () => {
         socket.addEventListener('message', event => {
             const newMessage = event.data;
             setMessages(prevMessages => [...prevMessages, newMessage]);
+
+            setTimeout(function () {
+                const messages = document.querySelectorAll(".message");
+                messages[messages.length - 1].scrollIntoView();
+                // Perform some other actions
+            }, 25);
         });
 
         socket.addEventListener('close', () => {
             console.log('WebSocket Disconnected!');
         });
-
 
         return () => {
             socket.close();
@@ -58,7 +64,8 @@ const Socket = () => {
     return (
         <div className={"bg-gray-800 w-full h-full p-0 m-0 box-border h-screen"}>
             <div className={"w-full h-full p-0 m-0 box-border flex justify-center items-center flex-col"}>
-                <div id={"messages"} className={" -p-4 bg-gray-900 br-5 text-center rounded-3xl overflow-y-auto text-white w-4/5 h-4/5 p-0"}>
+                <div id={"messages"}
+                     className={"transition-duration: 5s scroll-smooth m-4 p-6 bg-gray-900 br-5 text-center rounded-3xl overflow-y-auto text-white w-4/5 h-4/5 p-0"}>
                     {messages.map((message, index) => (
                         <div className={"message mt-2 font-sans text-2xl"} key={index}>{message}</div>
                     ))}
@@ -66,15 +73,21 @@ const Socket = () => {
 
                 <div className={"grow-0 flex flex-row gap-4 w-full justify-center items-center"}>
                     <div className={"flex justify-center items-center"}>
-                        <input className={"mt-4 p-2 truncate ... rounded-full text-white text-center border-0 outline-0 text-2xl bg-gray-700"} defaultValue={"Anonymous"} onKeyUp={(event) => event.key === 'Enter' && sendMessage()} onChange={handleNameChange}></input>
+                        <input
+                            className={"mt-4 p-2 truncate ... rounded-full text-white text-center border-0 outline-0 text-2xl bg-gray-700"}
+                            defaultValue={"Anonymous"} onKeyUp={(event) => event.key === 'Enter' && sendMessage()}
+                            onChange={handleNameChange}></input>
                     </div>
 
                     <div className={"flex w-1/3 justify-center items-center"}>
-                        <input id={"message-input"} className={"break-words w-full mt-4 p-2 truncate ...  rounded-full text-white text-center border-0 outline-0 text-2xl bg-gray-700"} placeholder={"Hey! Whats going on?"} onKeyUp={(event) => event.key === 'Enter' && sendMessage()} onChange={handleInputChange}></input>
+                        <input id={"message-input"}
+                               className={"break-words w-full mt-4 p-2 truncate ...  rounded-full text-white text-center border-0 outline-0 text-2xl bg-gray-700"}
+                               placeholder={"Hey! Whats going on?"}
+                               onKeyUp={(event) => event.key === 'Enter' && sendMessage()}
+                               onChange={handleInputChange}></input>
                     </div>
 
                 </div>
-
 
 
             </div>
