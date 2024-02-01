c69af0ffe260f2d86c144c617ef6bae9895bc551
Tapwatero <charliewoolf20@gmail.com>
2024-02-01 23:41:52 +0000

    new host wss

--- src/Socket.js
+++ src/Socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const initializeSocket = (cookies, setAuthenticated, setMessages) => {
-    const socket = io('wss://cloaked.onrender.com', {
+    const socket = io('wss://cloaked-backend.onrender.com', {
         'reconnection': true,
         'reconnectionDelay': 500,
         'reconnectionDelayMax' : 2500,
